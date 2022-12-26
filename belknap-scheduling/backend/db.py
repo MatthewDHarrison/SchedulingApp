914617ab5680508db56cf0bf2f75c8ebf04d1a7e
MatthewDHarrison <madoha5812@gmail.com>
2022-12-26 18:32:43 +1100

    simple login api function using flask-login, also simple register (doesn't check for duplicate usernames)

--- belknap-scheduling/backend/db.py
+++ belknap-scheduling/backend/db.py
@@ -3,10 +3,21 @@ import sqlite3
 conn = sqlite3.connect("schedule.sqlite")
 
 cursor = conn.cursor()
-sql_query = """ CREATE TABLE leaders (
-    id integer PRIMARY KEY,
-    name text NOT NULL,
-    position text NOT NULL
+# sql_query = """ CREATE TABLE leaders (
+#     id integer PRIMARY KEY,
+#     name text NOT NULL,
+#     position text NOT NULL
+# )"""
+
+# cursor.execute(sql_query)
+
+sql_query2 = """ CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username text NOT NULL,
+    password text NOT NULL
 )"""
 
-cursor.execute(sql_query)
+# sq3 = """insert into users values(0, 'xyz@gmail.com','XYZ123abc')"""
+
+cursor.execute(sql_query2)
+# cursor.execute(sq3)
--- belknap-scheduling/backend/flaskapi.py
+++ belknap-scheduling/backend/flaskapi.py
@@ -1,8 +1,35 @@
 from flask import Flask, request, jsonify
 import json
-import sqlite3 
+from flask import render_template, url_for, flash, request, redirect, Response
+import sqlite3
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+import secrets
+
 
 app = Flask(__name__)
+secret = secrets.token_urlsafe(32)
+app.secret_key = secret
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.authenticated = False    
+    def is_active(self):
+        return self.is_active()    
+    def is_anonymous(self):
+        return False    
+    def is_authenticated(self):
+        return self.authenticated    
+    def is_active(self):
+        return True    
+    def get_id(self):
+        return self.id
 
 def db_connection():
     conn = None
@@ -11,6 +38,40 @@ def db_connection():
     except sqlite3.error as e:
         print(e)
     return conn
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = db_connection()
+    curs = conn.cursor()
+    curs.execute("SELECT * from users where id = (?)",[user_id])
+    lu = curs.fetchone()
+    if lu is None:
+        return None
+    else:
+        return User(int(lu[0]), lu[1], lu[2])
+
+    
+@app.route("/users/login", methods=['POST'])
+def login():
+    if current_user.is_authenticated:
+        return f"User already autheticated"
+
+    login_username = request.form['username']
+    login_password = request.form['password']
+
+    conn = db_connection()
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM users where username = (?)", [login_username])
+
+    user = list(curs.fetchone())
+    Us = load_user(user[0])
+    if login_username == Us.username and login_password == Us.password:
+        login_user(Us, remember=True)
+        return f"Logged in successfully"
+    else:
+        return f"Login failed"
+
 
 @app.route('/leaders', methods=['GET', 'POST'])
 def leaders():
@@ -37,6 +98,23 @@ def leaders():
         cursor = conn.execute(sql, (new_name, new_position))
         conn.commit()
        
+        return f"Leader w/ id: {cursor.lastrowid} added to db"
+
+@app.route('/users/register', methods=['POST'])
+def usersRegister():
+    if request.method == 'POST':
+        conn = db_connection()
+        cursor = conn.cursor()
+
+        new_username = request.form['username']
+        new_password = request.form['password']
+
+        sql = """INSERT INTO users (username, password)
+                    VALUES (?, ?)"""
+        
+        cursor = cursor.execute(sql, (new_username, new_password))
+        conn.commit()
+        
         return f"Leader w/ id: {cursor.lastrowid} added to db"
 
 if __name__ == "__main__":
